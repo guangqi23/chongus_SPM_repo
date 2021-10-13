c454e10d1320f32f0cedcacd6b5fb8c57c2b5cd6
Big Bart Tok <72696034+tzekiattok@users.noreply.github.com>
2021-10-13 19:56:59 +0800

    Update Flask_TDD.py

--- code_example/Flask_TDD.py
+++ code_example/Flask_TDD.py
@@ -3,9 +3,8 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 import re
-from course import COURSE
-from course import app
-from unittest import TestCase
+from SectionMaterialController import app
+#from unittest import TestCase
 #from flask_cors import CORS
 
 
@@ -14,10 +13,10 @@ class FlaskTest(unittest.TestCase):
 
     def test_index(self):
         tester = app.test_client(self)
-        response = tester.get("/ALL_COURSES")
+        response = tester.get("http://localhost:5001/view_section_materials?section_id=0")
+        #raise exception if section id =0; else pass.
         statuscode = response.status_code
         self.assertEqual(statuscode,200)
-
 
 
 #https://www.youtube.com/watch?v=UZyZw4tYJMI
