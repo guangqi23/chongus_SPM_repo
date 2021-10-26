730ce58529d085a3598f898aa16f28b6588aa306
tzekiattok <72696034+tzekiattok@users.noreply.github.com>
2021-10-26 10:46:09 +0800

    transferred to lms file
    test

---
+++ html/lms/SectionMaterialController.py
@@ -0,0 +1,160 @@
+from flask import Flask, json, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from  section_material_quiz_data_access import SectionMaterialQuizDataAccess
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/lmsdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+CORS(app) 
+
+class SectionQuizController():
+    def create_materials(self, section_id):
+        pass
+
+#   View all materials in a section
+    def view_all_materials(self,section_id):
+        da = SectionMaterialQuizDataAccess()
+        materials = da.get_material_record_by_section(section_id)
+        return materials
+
+
+#   View all sections in a class
+    def view_all_sections(self, class_id):
+        da = SectionMaterialQuizDataAccess()
+        sections = da.get_sections_by_class(class_id)
+        return sections
+    
+#   Get quiz id of that section
+    def view_section_quiz(self,section_id):
+        da = SectionMaterialQuizDataAccess()
+        sections = da.get_section_quiz(section_id)
+        return sections
+
+#   Create section
+    def create_section(self,class_id,section_title):
+        da = SectionMaterialQuizDataAccess()
+        status = da.create_section(class_id,section_title)
+        return status
+
+#   Create Quizzes
+    def create_quiz(self,section_id,time_limit):
+        da = SectionMaterialQuizDataAccess()
+        status = da.create_quiz(section_id,time_limit)
+        return status
+
+#   View Quiz Questions
+    def get_quiz_questions(self,quiz_id):
+        da = SectionMaterialQuizDataAccess()
+        questions = da.get_quiz_questions(quiz_id)
+        return questions
+
+
+#   Create Quiz Questions
+    def create_quiz_questions(self, quiz_id,question_type,qorder,question):
+        da = SectionMaterialQuizDataAccess()
+        status = da.create_quiz(quiz_id,question_type,qorder,question)
+        return status
+
+#   Create Course Materials
+    def create_section_materials(self,section_id,material_title,material_content,material_type):
+        da = SectionMaterialQuizDataAccess()
+        status = da.create_material(section_id,material_title,material_content,material_type)
+        return status
+
+    
+
+
+#   Download Course Materials
+    #tbu after initializing S3
+
+        
+@app.route("/create_materials", methods=['GET'])
+def create_Materials(self):
+    pass
+
+
+############## View Functions ###############################
+@app.route("/view_section_materials", methods=['GET'])
+def view_Materials():
+    section_id = int(request.args.get('section_id', None))
+    da = SectionQuizController()
+    materials = da.view_all_materials(section_id)
+    return materials
+
+    
+@app.route("/view_class_sections", methods=['GET'])
+def view_Sections():
+    class_id = int(request.args.get('class_id', None))
+    da = SectionQuizController()
+    sections = da.view_all_sections(class_id)
+    return sections
+
+@app.route("/view_section_quiz", methods=['GET'])
+def view_Quiz():
+    section_id = int(request.args.get('section_id', None))
+    da = SectionQuizController()
+    quiz = da.view_section_quiz(section_id)
+    return quiz
+
+
+@app.route("/create_section", methods=['GET'])
+def create_section():
+    class_id = int(request.args.get('class_id', None))
+    section_title = str(request.args.get('section_title', None))
+    da = SectionQuizController()
+    status = da.create_section(class_id,section_title)
+    return status
+
+@app.route("/create_quiz", methods=['GET'])
+def create_quiz():
+    section_id = int(request.args.get('section_id', None))
+    time_limit = int(request.args.get('time_limit', None))
+    da = SectionQuizController()
+    status = da.create_quiz(section_id,time_limit)
+    return status
+    
+@app.route("/get_quiz_questions", methods=['GET'])
+def get_quiz_questions():
+    quiz_id = int(request.args.get('quiz_id', None))
+    da = SectionQuizController()
+    status = da.get_quiz_questions(quiz_id)
+    return status
+
+@app.route("/create_quiz_questions", methods=['GET'])
+def create_quiz_questions():
+    quiz_id = int(request.args.get('quiz_id', None))
+    question_type = str(request.args.get('quiz_id', None))
+    qorder = str(request.args.get('qorder', None))
+    question =str(request.args.get('question', None))
+    da = SectionQuizController()
+    status = da.create_quiz_questions(quiz_id,question_type,qorder,question)
+    return status
+
+
+@app.route("/view_quiz_questions", methods=['GET'])
+def get_qn():
+    qn_id = int(request.args.get('qn_id', None))
+    da =  SectionMaterialQuizDataAccess()
+    quiz = da.view_qn(qn_id)
+    return quiz
+
+@app.route("/create_section_materials", methods=['GET'])
+def create_section_materials():
+    section_id = int(request.args.get('section_id', None))
+    material_title = str(request.args.get('material_title', None))
+    material_content = str(request.args.get('material_content', None))
+    material_type = str(request.args.get('material_type', None))
+    da = SectionQuizController()
+    status = da.create_section_materials(section_id,material_title,material_content,material_type)
+    return status
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
---
+++ html/lms/section.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from sectionmaterials import SectionMaterials
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+CORS(app) 
+
+class Section(db.Model):
+    __tablename__ = 'sections'
+    section_id = db.Column(db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer)
+    section_title = db.Column(db.String(500))
+    
+
+    def json(self):
+        return {"section_id": self.section_id, "class_id": self.class_id,"section_title": self.section_title}
+
+    def get_class_id(self):
+        return self.class_id
+
+    def get_section_id(self):
+        return self.section_id
+
+    def get_section_title(self):
+        return self.section_title
+
+    def get_sections(self, section_id):
+        section = Section.query.filter_by(section_id=section_id).first()
+        return section
+
+    def get_section_all(self,class_id):
+        sections  = Section.query.filter_by(class_id=class_id)
+        #check if empty
+        count =0
+        for x in sections:
+            count+=1
+
+        if count!=0:
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": [section.json() for section in sections]
+                })
+        return jsonify(
+            {
+                "code": 404,
+                "message": "There are no section."
+            }
+        )
