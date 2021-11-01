c96f855deaa30004cd27f5ddf07fcd4c43443906
guangqi23 <56778377+guangqi23@users.noreply.github.com>
2021-11-01 13:34:29 +0800

    featWithdrawEnrolledCourses
    allowing hr to withdraw learners from enrolled courses

--- code_example/course_enrollment_controller.py
+++ code_example/course_enrollment_controller.py
@@ -5,7 +5,7 @@ from  course_enrollment_data_access import CourseEnrollmentDataAccess
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/lmsdb'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/lmsdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
--- html/lms/enrollment_controller.py
+++ html/lms/enrollment_controller.py
@@ -7,7 +7,7 @@ from course_prerequisites import Course_Prerequisites
 from classes import Classes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/lmsdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
