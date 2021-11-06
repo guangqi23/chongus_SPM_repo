d6a966f2d2fa4bf9cde62fef51d39db6b89e1889
guangqi23 <56778377+guangqi23@users.noreply.github.com>
2021-11-06 23:30:03 +0800

    style: to fix styling issues
    commented away trainer, trainer_assignment, ungraded_quiz_score,
    user,
    view_controller

--- html/lms/trainer.py
+++ html/lms/trainer.py
@@ -1,54 +1,54 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from user import User
-# from sqlalchemy.ext.declarative.api import declared_attr
-# from sqlalchemy import Column, Integer
-from trainer_assignment import Trainer_Assignment
-import models
+# from flask import Flask, request, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
+# from user import User
+# # from sqlalchemy.ext.declarative.api import declared_attr
+# # from sqlalchemy import Column, Integer
+# from trainer_assignment import Trainer_Assignment
+# import models
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB2'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/lmsdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB2'
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/lmsdb'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = models.db
+# db = models.db
 
-CORS(app) 
+# CORS(app) 
 
-class Trainer(User):
-    __tablename__ = 'TRAINERS'
+# class Trainer(User):
+#     __tablename__ = 'TRAINERS'
 
-    __mapper_args__ = {'polymorphic_identity': 'trainer'}
-    userid = db.Column(db.Integer, db.ForeignKey('users.userid'), primary_key=True)
+#     __mapper_args__ = {'polymorphic_identity': 'trainer'}
+#     userid = db.Column(db.Integer, db.ForeignKey('users.userid'), primary_key=True)
     
-    # def is_trainer(self, userid):
-    #     trnr = Trainer.query.filter_by(userid=userid).first()
-    #     return trnr
+#     # def is_trainer(self, userid):
+#     #     trnr = Trainer.query.filter_by(userid=userid).first()
+#     #     return trnr
 
-    def get_user_id(self):
-        return self.userid
+#     def get_user_id(self):
+#         return self.userid
     
-    def get_all_trainers(self):
-        trnr = Trainer()
-        return trnr.query.filter_by(designation='Senior Engineer').all()
+#     def get_all_trainers(self):
+#         trnr = Trainer()
+#         return trnr.query.filter_by(designation='Senior Engineer').all()
 
-    def get_assigned_classes(self,user_id):
+#     def get_assigned_classes(self,user_id):
         
-        classes = Trainer_Assignment.query.filter_by(userid=user_id)
+#         classes = Trainer_Assignment.query.filter_by(userid=user_id)
 
-        results_dict = {}
-        count = 1   
-        for eachClass in classes:
+#         results_dict = {}
+#         count = 1   
+#         for eachClass in classes:
             
-            msg = "class " + str(count)
-            u_id = eachClass.get_user_id()
-            co_id = eachClass.get_course_id()
-            cla_id = eachClass.get_class_id()
-            results_dict[msg] = {'course_id' : co_id, "class_id" : cla_id , "userid" : u_id}
-            count+=1
+#             msg = "class " + str(count)
+#             u_id = eachClass.get_user_id()
+#             co_id = eachClass.get_course_id()
+#             cla_id = eachClass.get_class_id()
+#             results_dict[msg] = {'course_id' : co_id, "class_id" : cla_id , "userid" : u_id}
+#             count+=1
 
-        return results_dict
+#         return results_dict
       
 
 
--- html/lms/user.py
+++ html/lms/user.py
@@ -1,45 +1,45 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-import models
+# from flask import Flask, request, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
+# import models
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB2'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:wangxingjie@spmdatabase.ca0m2kswbka0.us-east-2.rds.amazonaws.com:3306/LMSDB2'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = models.db
+# db = models.db
 
-CORS(app) 
+# CORS(app) 
 
-# flask implementation
-class User(db.Model):
-    __tablename__ = 'USERS'
-    __mapper_args__ = {
-        'polymorphic_identity':'user'
-    }
+# # flask implementation
+# class User(db.Model):
+#     __tablename__ = 'USERS'
+#     __mapper_args__ = {
+#         'polymorphic_identity':'user'
+#     }
 
-    userid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.Integer, nullable=False)
-    department = db.Column(db.String(100), nullable=False)
-    designation = db.Column(db.String(100), nullable=False)
+#     userid = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.Integer, nullable=False)
+#     department = db.Column(db.String(100), nullable=False)
+#     designation = db.Column(db.String(100), nullable=False)
 
-    # def __init__(self, user_id, name, email, department, designation):
-    #     self.user_id = user_id
-    #     self.name = name
-    #     self.email = email
-    #     self.department = department
-    #     self.designation = designation
+#     # def __init__(self, user_id, name, email, department, designation):
+#     #     self.user_id = user_id
+#     #     self.name = name
+#     #     self.email = email
+#     #     self.department = department
+#     #     self.designation = designation
 
-    def json(self):
-        return {"user_id": self.userid, "name": self.name, "email": self.email, "department": self.department, "designation": self.designation}
+#     def json(self):
+#         return {"user_id": self.userid, "name": self.name, "email": self.email, "department": self.department, "designation": self.designation}
 
-    def get_name(self):
-        return self.name
+#     def get_name(self):
+#         return self.name
 
-    def get_user_id(self):
-        return self.userid
+#     def get_user_id(self):
+#         return self.userid
 
-    # def is_hr(self, userid):
-    #     user = User.query.filter_by(userid=userid).first()
-    #     return user
+#     # def is_hr(self, userid):
+#     #     user = User.query.filter_by(userid=userid).first()
+#     #     return user
